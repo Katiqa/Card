462d643cf8942df8d1686edb3e07b46887758c61
Kati <katyushka.zykova.2015@mail.ru>
2024-01-30 19:32:58 +0300

    Task 1

--- src/test/java/CardSeleniumTest.java
+++ src/test/java/CardSeleniumTest.java
@@ -9,6 +9,9 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.List;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CardSeleniumTest {
     private WebDriver driver;
 
@@ -25,6 +28,7 @@ public class CardSeleniumTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:7777");
 
     }
 
@@ -37,13 +41,13 @@ public class CardSeleniumTest {
 
     @Test
     void ValidateData() throws InterruptedException {
-        driver.get("http://localhost:7777");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Иван Иванов");
-        elements.get(1).sendKeys("+79999956258");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
-        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Петр Петров");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79999956258");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        var elementText = driver.findElement(By.cssSelector("[data-test-id=order-success]"));
+        var text = elementText.getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+        assertTrue(elementText.isDisplayed());
     }
 }
