08b5199e3ef69f7d8808e8c7c50d3a9113a5b648
Kati <katyushka.zykova.2015@mail.ru>
2024-01-30 18:31:15 +0300

    Try

--- src/test/java/CardSeleniumTest.java
+++ src/test/java/CardSeleniumTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 import java.util.Set;
@@ -21,8 +22,13 @@ public class CardSeleniumTest {
 
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
+
 
     @AfterEach
     void tearDown() {
@@ -31,7 +37,7 @@ public class CardSeleniumTest {
     }
 
     @Test
-    void ValidateData(){
+    void ValidateData() {
         driver.get("http://localhost:7777/");
     }
 }
