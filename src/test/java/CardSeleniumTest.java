9ad2018c4a7f0cff12bc1cd8aa5c1f3267f59a82
Kati <katyushka.zykova.2015@mail.ru>
2024-01-30 19:07:44 +0300

    First Test

--- src/test/java/CardSeleniumTest.java
+++ src/test/java/CardSeleniumTest.java
@@ -1,8 +1,5 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -22,11 +19,13 @@ public class CardSeleniumTest {
 
     @BeforeEach
     void setUp() {
+//        driver = new ChromeDriver();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+
     }
 
 
@@ -37,7 +36,14 @@ public class CardSeleniumTest {
     }
 
     @Test
-    void ValidateData() {
+    void ValidateData() throws InterruptedException {
         driver.get("http://localhost:7777");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Иван Иванов");
+        elements.get(1).sendKeys("+79999956258");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 }
